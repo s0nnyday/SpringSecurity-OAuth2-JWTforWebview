13d631dc2c8210bb4a694856e43884e4f3fce793
s0nnyday <lijnar100@naver.com>
2024-03-28 14:21:35 +0900

    init

---
+++ src/main/java/com/example/securityoauthjwtwebview/SecurityoauthjwtwebviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityoauthjwtwebview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityoauthjwtwebviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityoauthjwtwebviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securityoauthjwtwebview/SecurityoauthjwtwebviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityoauthjwtwebview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityoauthjwtwebviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
